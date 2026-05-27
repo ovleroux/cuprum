ddbb7401f4148315d75d2fdfacba1f4467a71c32
Ofentse le Roux <ofentse.leroux@gmail.com>
2026-05-27 18:53:24 +0200

    localfont

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,11 @@
 import type { Metadata } from "next";
-import Sandvik_Sans  from "next/font/local";
+import localFont  from "next/font/local";
 // import Sandvik_Sans_Headline from "next/font/local";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 // import { Variable } from "lucide-react";
 
-const sandvikText = Sandvik_Sans({
+const sandvikText = localFont({
   src: [
     {
       path: './fonts/sandvik-sans/SandvikSansText-Light.woff2',
